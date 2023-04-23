201fb13ab22fef56cbc02d80183b367a713dc533
Pravin Bhagwat <85389806+pravinbhagwat@users.noreply.github.com>
2023-04-23 19:28:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const student = {
+  name: 'John Doe'
+};
+
+Object.prototype.getKeys = function() {
+  // Return an array of all the keys in the object
+  return Object.keys(this);
+};
+
+console.log(student.getKeys());
